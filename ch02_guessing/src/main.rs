b8f01fd772b6431d8a5812c8ab793f2943694087
Daryl Lee <daryl@daryllee.com>
2025-04-05 21:01:29 -0600

    Ready to add randomness

--- ch02_guessing/src/main.rs
+++ ch02_guessing/src/main.rs
@@ -5,6 +5,8 @@ fn main() {
     let mut guess = String::new();
     io::stdin()
         .read_line(&mut guess)
+        // See Ch. 9 for better error handling
+        // Here 'msg:' is added by VSCode
         .expect("Failed to read line");
     println!("You guessed: {}", guess);
 }
